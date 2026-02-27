92f914c244b0da12bca9aa65df597cf4ab0f9dd4
mshriner <marshriner@gmail.com>
2026-02-26 20:58:58 -0600

    make routes not lazyload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 
+import { Home } from './components/home/home';
+import { ResumePage } from './components/resume/resume-page';
 import { APP_ROUTES } from './models/constants';
 
 export const routes: Routes = [
   {
     path: APP_ROUTES.HOME,
-    loadComponent: () => import('./components/home/home').then((m) => m.Home),
+    component: Home,
   },
   {
     path: APP_ROUTES.RESUME,
-    loadComponent: () => import('./components/resume/resume-page').then((m) => m.ResumePage),
+    component: ResumePage,
   },
   { path: '', redirectTo: `/${APP_ROUTES.HOME}`, pathMatch: 'full' },
   { path: '**', redirectTo: `/${APP_ROUTES.HOME}` },
